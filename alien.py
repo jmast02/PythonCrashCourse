b1abbad99a48b52ec89921c9c27b5273a403ebfa
Jonathan Mast <jmast2014@fau.edu>
2019-04-11 17:11:57 -0400

    Just testing Pycharm/Github

---
+++ alien.py
@@ -0,0 +1,25 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('Player + 5 points.')
+
+
+
+second_alien_color = 'red'
+
+if second_alien_color == 'green':
+    print('player + 5 points.')
+elif second_alien_color == 'yellow':
+    print('player + 10 points.')
+elif second_alien_color == 'red':
+    print('player points + 15.')
+
+
+
+
+
+
+
+
+
+
